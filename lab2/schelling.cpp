8afc9e3ebd79012ed47649b58387d1a21d61e137
lukoshkin <lukoshkin@phystech.edu>
2019-05-12 14:53:35 +0300

    Fix tons of errors

--- lab2/schelling.cpp
+++ lab2/schelling.cpp
@@ -2,16 +2,18 @@
 #include <algorithm>
 #include "mpi.h"
 
+#define CHECK true
+
 std::random_device seed;
 std::mt19937 gen{seed()};
 using uid = std::uniform_int_distribution<int>;
 
 class District {
-    int size_x;
     int x_max;
 
     int size_y;
     int y_max;
+    int y0;
 
     float tol;
 
@@ -21,40 +23,42 @@ class District {
     int count;
     int white;
     int * mask;
+    int * moving;
 
     int si(int, int); // safe index
 
     public:
-      int * moving;
 
       District(int, int, int, int, float);
       ~District();
         
       void initialize(int *, int *);
       void zeroIntent();
-      void allLookAround();
       void quickLookFrom(int, int);
+      void allButBottomLookAround();
+      void bottomOnlyLookAround();
       std::pair<int,int> findMoving();
-      void settle(int);
+      void settleResidents(int);
+      int  conductCensus();
 };
 
-
-District::District(int side_x, int x_sup, 
-                   int side_y, int y_sup, 
-                   float coeff) {
+District::District(int sup_X, 
+                   int Y_size, int sup_Y, 
+                   int id,   float coeff) {
     count = 0;
     white = 0;
 
     tol = coeff;
 
-    size_x = side_x;
-    size_y = side_y;
+    size_y = Y_size;
 
-    x_max  = x_sup;
-    y_max  = y_sup;
+    x_max  = sup_X;
+    y_max  = sup_Y;
 
-    mask   = (int *)malloc(size_x * size_y * sizeof(int));
-    moving = (int *)malloc(size_x * size_y * sizeof(int));
+    y0 = id * size_y;
+
+    mask   = (int *)malloc(x_max * size_y * sizeof(int));
+    moving = (int *)malloc(x_max * size_y * sizeof(int));
 }
 
 District::~District() {
@@ -64,9 +68,9 @@ District::~District() {
 
 int District::si(int i, int max) {
     if (i < 0)
-        return max - 1;
-    else if (i > max)
-        return 0;
+        return max - i;
+    else if (i > max - 1)
+        return i - max;
     return i;
 }
 
@@ -75,17 +79,17 @@ void District::initialize
     houses = huts;
     intent = will;
 
-    std::uniform_int_distribution<int> D(0, 1);
-    for (int i=0; i<size_y; ++i) {
-        for (int j=0; j<size_x; ++j)
-            houses[i * size_x + j] = D(gen);
+    uid D(0, 1);
+    for (int i=y0; i<y0+size_y; ++i) {
+        for (int j=0; j<x_max; ++j)
+            houses[i * x_max + j] = D(gen);
     }
 }
 
 void District::zeroIntent() {
-    for (int i=0; i<size_y; ++i) {
-        for (int j=0; j<size_x; ++j) 
-            intent[i * size_x + j] = 0;
+    for (int i=y0; i<y0+size_y; ++i) {
+        for (int j=0; j<x_max; ++j) 
+            intent[i * x_max + j] = 0;
     }
 }
 
@@ -110,19 +114,24 @@ void District::quickLookFrom(int i, int j) {
     intent[si(i-1, y_max) * x_max + si(j+1, x_max)] += match3; 
 }
 
-void District::allLookAround() {
-    for (int i=0; i<size_y; ++i) {
-        for (int j=0; j<size_x; ++j)
+void District::allButBottomLookAround() {
+    for (int i=y0; i<y0+size_y-1; ++i) {
+        for (int j=0; j<x_max; ++j)
             quickLookFrom(i,j); 
     } 
 }
 
+void District::bottomOnlyLookAround() {
+    for (int j=0; j<x_max; ++j)
+        quickLookFrom(y0+size_y-1, j); 
+}
+
 std::pair<int,int> District::findMoving() {
     int address;
-    for (int i=0; i<size_y; ++i) {
-        for (int j=0; j<size_x; ++j) {
-            address = i * size_x + j;
-            if ((double)intent[address] > 8 * tol) {
+    for (int i=y0; i<y0+size_y; ++i) {
+        for (int j=0; j<x_max; ++j) {
+            address = i * x_max + j;
+            if ((float)intent[address] > 8 * tol) {
                 white += houses[address];
                 moving[count] = address;
                 count++;
@@ -132,10 +141,10 @@ std::pair<int,int> District::findMoving() {
     return std::make_pair(count, white);
 }
 
-void District::settle(int white) {
-    for (int i=0; i<white; ++i) 
+void District::settleResidents(int ones) {
+    for (int i=0; i<ones; ++i) 
         mask[i] = 1;
-    for (int i=white; i<count; ++i)
+    for (int i=ones; i<count; ++i)
         mask[i] = 0;
 
     std::shuffle(mask, mask+count, gen);
@@ -143,90 +152,156 @@ void District::settle(int white) {
         houses[moving[i]] = mask[i];
 
     count = 0;
+    white = 0;
 }
 
+int District::conductCensus() {
+    int ones(0);
+    for (int i=y0; i<y0+size_y; ++i) {
+        for (int j=0; j<x_max; ++j)
+            ones += houses[i * x_max + j];
+    }
+    return ones;
+}
 
-
+/////////////////////////// MAIN /////////////////////////////////////
 int main(int argc, char ** argv) {
-    int size_x(200), size_y(20), 
-         x_max(200), y_max(200);
     int n_steps = std::atoi(argv[1]);
     float coeff = std::atof(argv[2]);
-
     MPI_Init(&argc, &argv);
-    MPI_Comm comm = MPI_COMM_WORLD;
 
-    int * houses;
-    int * intent;
-    MPI_Win win;
+    MPI_Comm shmcomm;
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 
+                                  0, MPI_INFO_NULL, &shmcomm);
 
-    MPI_Win_allocate(x_max * y_max * sizeof(int), sizeof(int), 
-                          MPI_INFO_NULL, comm, &houses, &win);
-    MPI_Win_allocate(x_max * y_max * sizeof(int), sizeof(int), 
-                          MPI_INFO_NULL, comm, &intent, &win);
-    int pid, num_proc;
-    MPI_Comm_rank(comm, &pid);
-    MPI_Comm_size(comm, &num_proc);
+    int pid, num_proc, world_num_proc;
+    MPI_Comm_rank(shmcomm, &pid);
+    MPI_Comm_size(shmcomm, &num_proc);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_num_proc);
+
+    if (world_num_proc != num_proc) MPI_Abort(MPI_COMM_WORLD, 1);
     
-    District region(size_x, x_max, size_y, y_max, coeff);
-    region.initialize(houses + pid * size_y,
-                      intent + pid * size_y);
-    MPI_Barrier(comm);
+    int x_max(200), size_y(20);
+    int y_max = num_proc * size_y;
 
-    int white, whites;
-    int * counts;
-    int * decision;
+    MPI_Win win;
+    int * baseptr, * houses, * intent;
+
+    int loc_size = (pid == 0) ? 2 * x_max * y_max : 0;
+
+    MPI_Win_allocate_shared(loc_size * sizeof(int), sizeof(int), 
+                          MPI_INFO_NULL, shmcomm, &baseptr, &win);
+    if (pid != 0) {
+        int disp_unit;
+        MPI_Aint win_size;
+        MPI_Win_shared_query(win, 0, &win_size, &disp_unit, &baseptr);
+    }
+
+    houses = baseptr;
+    intent = baseptr + x_max * y_max;
+    
+    double time, max_time;
+    time = - MPI_Wtime();
+
+    MPI_Win_lock_all(MPI_MODE_NOCHECK, win);
+
+    District region(x_max, size_y, y_max, pid, coeff);
+    region.initialize(houses, intent);
+
+#if CHECK
+    int population_census;
+    int regpop_census = region.conductCensus();
+    MPI_Reduce(&regpop_census, &population_census, 1, 
+                      MPI_INT, MPI_SUM, 0, shmcomm);
+    if (pid == 0)
+        printf("number of whites BEFORE: %i\n", 
+                population_census);
+#endif
+
+    MPI_File fh;
+    MPI_File_open(shmcomm, "initial.bin",
+                  MPI_MODE_CREATE | MPI_MODE_WRONLY,
+                  MPI_INFO_NULL, &fh);
+    MPI_File_set_view(fh, pid * x_max * size_y * sizeof(int),
+                      MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
+    MPI_File_write(fh, houses + pid * x_max * size_y, 
+                    x_max * size_y, MPI_INT, MPI_STATUS_IGNORE);
+    MPI_File_close(&fh);
+
+    int white, sum_white;
+    int * counts, * decision;
+    std::pair<int,int>  local_rebels;
 
     if (pid == 0) {
         counts   = (int *)malloc(num_proc * sizeof(int));
         decision = (int *) malloc(num_proc * sizeof(int));
     }
-    std::pair<int,int>  local_rebels;
-
-    MPI_File fh;
-    MPI_File_open(comm, "initial.bin",
-                  MPI_MODE_CREATE | MPI_MODE_WRONLY,
-                  MPI_INFO_NULL, &fh);
-    MPI_File_set_view(fh, pid * size_x * size_y * sizeof(int),
-                      MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
-    MPI_File_write(fh, houses + pid * size_y, 
-                    size_x * size_y, MPI_INT, MPI_STATUS_IGNORE);
-    MPI_File_close(&fh);
-
     for (int t=0; t<n_steps; ++t) {
         region.zeroIntent();
-        MPI_Barrier(comm);
-        region.allLookAround();
+        region.allButBottomLookAround();
+
+        MPI_Win_sync(win);
+        MPI_Barrier(shmcomm);
+
+        region.bottomOnlyLookAround();
         local_rebels = region.findMoving();
 
         MPI_Gather(&local_rebels.first, 1, MPI_INT, 
-                        counts, 1, MPI_INT, 0, comm);
-        MPI_Reduce(&local_rebels.second, &whites, 1, 
-                          MPI_INT, MPI_SUM, 0, comm);
+                        counts, 1, MPI_INT, 0, shmcomm);
+        MPI_Reduce(&local_rebels.second, &sum_white, 1, 
+                          MPI_INT, MPI_SUM, 0, shmcomm);
         if (pid == 0) {
-           for (int i=0; i<num_proc; ++i) {
-                uid D(0, std::min(counts[i], whites));
+            int C, min, max;
+            C = std::accumulate(counts, counts + num_proc, 0);
+
+            //one can also create an array of indices from 0 to num_proc,
+            //shuffle it, and substitute the looping over the ordered 
+            //sequence with the iteration over the resulting array 
+            for (int i=0; i<num_proc; ++i) {
+                C -= counts[i];
+                max = std::min(counts[i], sum_white);
+                min = std::fabs(std::min(0, C - sum_white));
+                uid D(min, max);
+                
                 decision[i] = D(gen);
-                whites -= decision[i];
+                sum_white -= decision[i];
             }
         }
         MPI_Scatter(decision, 1, MPI_INT, &white, 
-                              1, MPI_INT, 0, comm);
-        region.settle(white);
+                              1, MPI_INT, 0, shmcomm);
+        region.settleResidents(white);
     }
-    MPI_File_open(comm, "resulting.bin",
+    MPI_Win_unlock_all(win);
+
+#if CHECK
+    population_census = 0;
+    regpop_census = region.conductCensus();
+    MPI_Reduce(&regpop_census, &population_census, 1, 
+                      MPI_INT, MPI_SUM, 0, shmcomm);
+    if (pid == 0)
+        printf("___..__..__..___  AFTER: %i\n", 
+                population_census);
+#endif
+
+    MPI_File_open(shmcomm, "resulting.bin",
                   MPI_MODE_CREATE | MPI_MODE_WRONLY,
                   MPI_INFO_NULL, &fh);
-    MPI_File_set_view(fh, pid * size_x * size_y * sizeof(int),
+    MPI_File_set_view(fh, pid * x_max * size_y * sizeof(int),
                       MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
-    MPI_File_write(fh, houses + pid * size_y, 
-                    size_x * size_y, MPI_INT, MPI_STATUS_IGNORE);
+    MPI_File_write(fh, houses + pid * x_max * size_y, 
+                    x_max * size_y, MPI_INT, MPI_STATUS_IGNORE);
     MPI_File_close(&fh);
 
+    time += MPI_Wtime();
+    MPI_Reduce(&time, &max_time, 1, 
+                      MPI_DOUBLE, MPI_MAX, 0, shmcomm);
     if (pid == 0) {
         free(counts);
         free(decision);
+        printf("\nElapsed time: %f s\n", time);
     }
     MPI_Win_free(&win);
+    MPI_Comm_free(&shmcomm);
+
     MPI_Finalize();
 }
